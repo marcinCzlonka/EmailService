02e59a39fd7348e58093eb842d9d7b9aeb785ab8
marcinczlonka <marcin.czlonka@softwaremind.com>
2020-08-28 11:24:34 +0200

    Create Entities and Context

---
+++ EmailService/Database/Entities/Email.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmailService.Database.Entities
+{
+    public class Email
+    {
+        public int Id { get; set; }
+        public EmailAddress Sender { get; set; }
+        public virtual ICollection<EmailAddress> Recipients { get; set; }
+        public bool Send { get; set; }
+        public string Text { get; set; }
+
+        
+    }
+}
